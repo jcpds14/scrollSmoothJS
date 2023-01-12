d5633a4d4e353dfa57fea913868e387e34f5defd
Jean Carlos <jcpds14@gmail.com>
2023-01-12 17:56:04 -0300

    testing scroll smooth with pure JS

---
+++ app.js
@@ -0,0 +1,24 @@
+function goToHome() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+function goToInfo() {
+    window.scrollTo({
+        top: 622,
+        behavior: 'smooth'
+    });
+}
+function goToContact() {
+    window.scrollTo({
+        top: 1237,
+        behavior: 'smooth'
+    });
+}
+function goToAbout() {
+    window.scrollTo({
+        top: 1868,
+        behavior: 'smooth'
+    });
+}
